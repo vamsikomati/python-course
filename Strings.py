c5a02630214df2202d550b51ce903e06fbdeda6d
vamsikomati <mail2vamsi.happy@gmail.com>
2023-06-08 21:24:44 -0400

    PYT-31:Create one line code for each function of str class

--- Strings.py
+++ Strings.py
@@ -1,3 +1,27 @@
 string = "python learning daily"
 x = string.__add__ (' at 9:30')
 print (x)
+
+string = "Mac OS 16"
+x = string.endswith("16")
+print (x)
+
+string = "Python Training"
+x = string.lower()
+print (x)
+
+string = "Python Training"
+x = string.upper()
+print (x)
+
+string = "python learning daily"
+x = string.count ("python")
+print (x) 
+
+string = "python learning daily"
+x = string.find("python")
+print (x)
+
+string = "PYTHON LEARNING DAILY"
+x = string.isupper()
+print (x)
